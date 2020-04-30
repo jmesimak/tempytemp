4aee814073eca1f6dc1319e60d4a216cc53bf40f
Jerry Mesimaki <jerry.mesimaki@gmail.com>
2020-04-30 19:04:14 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 function App() {
+  const [temps, setTemps] = useState([]);
+
+  useEffect(() => {
+    const getTemps = async () => {
+      const response = await fetch("http://nullhammer.fi:3000/temps");
+      const data = await response.json();
+      setTemps(data);
+      console.log(data);
+    };
+
+    getTemps();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LineChart
+        width={500}
+        height={300}
+        data={temps}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="time" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="temperature"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="humidity" stroke="#82ca9d" />
+      </LineChart>
     </div>
   );
 }
